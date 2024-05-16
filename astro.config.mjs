b2ba9ce9f7ee8e759f519def5948196648e8833c
Eduard Alberdi Marin <eal@novatec-gmbh.de>
2024-05-16 10:11:15 +0200

    Deploy portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://edualberdi.github.io',
-    output: 'static',
-    outDir: './docs',
-    buildOptions: {
-       assets: 'astro'
-    },
+    integrations: [
+        sitemap(),
+    ],
 });
